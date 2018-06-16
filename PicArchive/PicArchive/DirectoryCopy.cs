8e9eb5cd7d4b433ac7d9548fec7f499b988417c3
Daniel Korsah <dkorsah15@aol.co.uk>
2018-06-16 02:56:09 +0100

    Upload The program to github.

---
+++ PicArchive/PicArchive/DirectoryCopy.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace PicArchive
+{
+    public static class DirectoryCopy
+    {
+        public static void Copy(string startLocation, string destination, bool recursive)
+        {
+            // Get the subdirectories for the specified directory.
+            DirectoryInfo dir = new DirectoryInfo(startLocation);
+
+            if (!dir.Exists)
+            {
+                throw new DirectoryNotFoundException(
+                    "Source directory does not exist or could not be found: "
+                    + startLocation);
+            }
+
+            DirectoryInfo[] dirs = dir.GetDirectories();
+            // If the destination directory doesn't exist, create it.
+            if (!Directory.Exists(destination))
+            {
+                Directory.CreateDirectory(destination);
+            }
+
+            // Get the files in the directory and copy them to the new location.
+            FileInfo[] files = dir.GetFiles();
+            foreach (FileInfo file in files)
+            {
+                if (!File.Exists(Path.Combine(destination, file.Name)))
+                {
+                    string temppath = Path.Combine(destination, file.Name);
+                    file.CopyTo(temppath, false);
+                }
+            }
+
+            // If copying subdirectories, copy them and their contents to new location.
+            if (recursive)
+            {
+                foreach (DirectoryInfo subdir in dirs)
+                {
+                    //little modification mum requested
+                    if(subdir.FullName.ToLower().Contains("mum"))
+                    {
+                        string temppath = Path.Combine(destination, subdir.Name);
+                        Copy(subdir.FullName, temppath, recursive);
+                    }
+                    
+                }
+            }
+        }
+    }
+}
---
+++ PicArchive/PicArchive/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+
+namespace PicArchive
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string from = Environment.GetFolderPath(Environment.SpecialFolder.MyPictures);
+            string to = @"Picture_Backup\";
+
+            DirectoryCopy.Copy(from, to, true);
+        }
+    }
+}
